bac426c4c2b7f4f3ede836ed897e133238199d74
rsitdikov <rsitdikov.usp@gmail.com>
2020-01-12 02:51:42 +0500

    Коммиты на решения задач.[#220246]

---
+++ chapter_001/src/test/java/ru/job4j/condition/PointTest.java
@@ -0,0 +1,14 @@
+package ru.job4j.condition;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class PointTest {
+    @Test
+    public void distance () {
+        int in1 = -6, in2 = 0, in3 = 0, in4 = 8;
+        double expected = 10.0;
+        double out = Point.distance(in1, in2, in3, in4);
+        Assert.assertEquals(expected, out, 0.1);
+    }
+}
