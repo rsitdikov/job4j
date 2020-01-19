631a00a7af25ec588deb7bec67e096ec8891a0ce
rsitdikov <rsitdikov.usp@gmail.com>
2020-01-19 11:20:58 +0500

    JavaDoc. Code convention[#220332]

--- chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
+++ chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
@@ -1,26 +1,56 @@
 package ru.job4j.calculator;
 
+/**
+ * Calculate.
+ * @author Ramil Sitdikov
+ */
+
 public class Calculator {
+
+    /**
+     * Method add
+     * @param first - augend
+     * @param second - addend
+     */
 
     public static void add(double first, double second) {
         double result = first + second;
         System.out.println(first + "+" + second + " = " + result);
     }
+    /**
+     * Method div
+     * @param first - divisible
+     * @param second - divisor
+     */
 
     public static void div(double first, double second) {
         double result = first / second;
         System.out.println(first + "/" + second + " = " + result);
     }
+    /**
+     * Method multiply
+     * @param first - multiplier
+     * @param second - multiplicand
+     */
 
     public static void multiply(double first, double second) {
         double result = first * second;
         System.out.println(first + "*" + second + " = " + result);
     }
+    /**
+     * Method subtract
+     * @param first - minuend
+     * @param second - subtrahend
+     */
 
     public static void subtract(double first, double second) {
         double result = first - second;
         System.out.println(first + "-" + second + " = " + result);
     }
+    /**
+     * Method main
+     * @param args - args
+     */
 
     public static void main(String[] args) {
         add(1, 1);
