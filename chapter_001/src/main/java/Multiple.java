fa80a76c87f4e3fd905eb57202741084490311a1
rsitdikov <rsitdikov.usp@gmail.com>
2021-02-02 02:48:47 +0500

    Исправления

--- chapter_001/src/main/java/Multiple.java
+++ chapter_001/src/main/java/Multiple.java
@@ -1,8 +1,8 @@
 public class Multiple {
     public static void main(String[] args) {
-        for (int first = 1; first < 10; first ++) {
-            for (int second = 1; second < 10; second ++) {
-                System.out.println(String.format("%d x %d = %d", first, second, first*second));
+        for (int first = 1; first < 10; first++) {
+            for (int second = 1; second < 10; second++) {
+                System.out.println(String.format("%d x %d = %d", first, second, first * second));
             }
             System.out.println();
         }
